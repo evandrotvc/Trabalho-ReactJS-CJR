3f50df0c1225579de0c5c651c2fbe2ab76f614c4
natalylacerda <nataly.oliveira@cjr.org.br>
2020-05-27 13:14:54 -0300

    Acessa a API e mostra pokemons na tela

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import api from './api';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Hello world</h1>
-    </div>
-  );
+class App extends Component{
+
+  state = {
+    pokemons: [],
+  }
+
+  async componentDidMount(){
+    const response = await api.get('/pokemons');
+
+    console.log (response.data); 
+
+    this.setState({ pokemons: response.data.data});
+  }
+
+  render(){
+
+    const { pokemons } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Pokemons</h1>
+        {pokemons.map(pokemon => (
+          <span key={pokemon.id}>
+            <img src = {pokemon.image_url}></img>
+            <span>              
+              {pokemon.name}
+            </span>
+
+          </span>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
