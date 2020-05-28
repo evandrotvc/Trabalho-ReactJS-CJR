7307dad76cdbaebcb2168d9075adc32f4cf63bac
natalylacerda <nataly.oliveira@cjr.org.br>
2020-05-28 14:01:02 -0300

    Adiciona botões das páginas (de vdd dessa vez)

--- src/App.js
+++ src/App.js
@@ -6,34 +6,69 @@ class App extends Component{
 
   state = {
     pokemons: [],
+    pokemonInfo: {},
+    pages: 1,
   }
 
-  async componentDidMount(){
-    const response = await api.get('/pokemons');
+  componentDidMount(){
+    this.loadPokemon();
+  }
+
+  loadPokemon = async (pages = 1) => {
+    const response = await api.get(`/pokemons?page=${pages}`);
 
     console.log (response.data); 
 
-    this.setState({ pokemons: response.data.data});
+    const {data, ...pokemonInfo} = response.data;
+
+    this.setState({ pokemons: data, pokemonInfo});
+  }
+
+  prevPage = () => {
+    const {pages, pokemonInfo} = this.state;
+
+    if (pokemonInfo.prev_page === null) return;
+
+    const pageNum = pages - 1;
+
+    this.setState({pages: pageNum});
+    
+    this.loadPokemon(pageNum);
+  }
+
+  nextPage = () => {
+    const {pages, pokemonInfo} = this.state;
+
+    if (pokemonInfo.next_page === null) return;
+
+    const pageNum = pages + 1;
+
+    this.setState({pages: pageNum});
+
+    this.loadPokemon(pageNum);
   }
 
   render(){
 
     const { pokemons } = this.state;
-
+    
     return (
       <div className="App">
         <h1>Pokemons</h1>
+        <div className= "pokedex">
         {pokemons.map(pokemon => (
-          <span key={pokemon.id}>
-            <img src = {pokemon.image_url}></img>
+          <span key={pokemon.id} className="pokemonCard">
+            <img src = {pokemon.image_url} alt="pokemon"></img>
             <span>              
               {pokemon.name}
             </span>
           </span>
         ))}
-
-          {/* <button onClick= {this.prevPage}>Voltar</button>
-          <button onClick= {this.nextPage}>Avançar</button> */}
+        </div>
+        <div className="pageButtons">
+          <button onClick= {this.prevPage}>Voltar</button>
+          <button onClick= {this.nextPage}>Avançar</button>
+        </div>
       </div>
     );
   }
