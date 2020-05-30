5a387244c0bba070171bd1b3804d18dbdd8278e4
natalylacerda <nataly.oliveira@cjr.org.br>
2020-05-30 12:34:13 -0300

    Muda backgroud color do pokemon segundo o seu tipo

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,19 +1,19 @@
 import React from "react";
 import './Modal.css'
 
-const Modal = ({ id = 'modal', setPokemonProfile, children}) => {
+const Modal = ({ id = 'modal', setPokemonProfile}) => {
 
     const handleOutsideClose = (e) => {
         if (e.target.id === id) setPokemonProfile (false);
     }
 
-    return <div id= {id} onClick={handleOutsideClose} className="container">
+    return <div id={id} onClick={handleOutsideClose} className="container">
         
         <div  className= "modal">
 
             <button onClick={() => setPokemonProfile(false)} className= "close">X</button>
             <div className= "perfilPokemon">
-                {children}
+                <h1>Perfil</h1>
             </div>
 
         </div>
--- src/pages/Pokemons/Pokemon.js
+++ src/pages/Pokemons/Pokemon.js
@@ -55,6 +55,59 @@ class App extends Component{
 
   handlePokemonProfile = (show) => {this.setState({setPokemonProfile:show})}
 
+  handleColor = (type) => {
+    switch (type) {
+      case 'grass':
+          return '#9bcc50';
+      case 'fire':
+          return '#fd7d24';
+      case 'poison':
+          return '#b97fc9';
+      case 'flying':
+          return '#3dc7ef';
+      case 'water':
+          return '#4592c4';
+      case 'bug':
+          return '#729f3f';
+      case 'normal':
+          return '#a4acaf';
+      case 'electric':
+          return '#eed535';
+      case 'ground':
+          return '#ab9842';
+      case 'fairy':
+          return '#fdb9e9';
+      case 'fighting':
+          return '#d56723';
+      case 'psychic':
+          return '#f366b9';
+      case 'rock':
+          return '#a38c21';
+      case 'steel':
+          return '#9eb7b8';
+      case 'ice':
+          return '#51c4e7';
+      case 'ghost':
+          return '#7b62a3';
+      case 'dragon':
+          return '#f16e57';
+      case 'dark':
+          return '#707070';
+      default:
+          return;
+    }
+  }
+
+  handleType = (string_type) => {
+    let arr_type = string_type.split(';');
+    if (arr_type.length == 1){
+      return this.handleColor(arr_type[0]);      
+    }
+    else{
+      return `linear-gradient(to bottom, ${this.handleColor(arr_type[0])}, ${this.handleColor(arr_type[1])})`
+    }
+  }
+
   render(){
 
     const { pokemons } = this.state;
@@ -72,6 +125,7 @@ class App extends Component{
           <div key={pokemon.id} 
           onClick={() => this.setState({setPokemonProfile: true})}
            className="pokemonCard"
+           style =  {{background: this.handleType(pokemon.kind)}}
            >
            {/* <Link to= {`/pokemons/${pokemon.name}`}> */}
             <img src = {pokemon.image_url} alt="pokemon"></img>
