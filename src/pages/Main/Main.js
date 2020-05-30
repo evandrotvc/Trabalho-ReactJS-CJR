e667fa3dd87d819ab19e2f917949cbdbca8c9d84
JoaoPedroCNMota <joaopedrocnmota@gmail.com>
2020-05-30 19:54:19 -0300

    Login_User + Merges

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,36 +1,33 @@
-import React from 'react';
-import { Router } from "@reach/router";
+// import React from 'react';
+// import { Router } from "@reach/router";
 
-import Home from '../Home';
-import PerfilPokemon from '../PerfilPokemon/PerfilPokemon'
-import Header from '../../Components/Header/Header';
-import Login from '../Login';
-import Pokemon from '../Pokemons/Pokemon';
-<<<<<<< HEAD
-import NotFound from '../NotFound';
-=======
-import Footer from '../../Components/Footer'
->>>>>>> origin/Merges
-import './Main.css';
-import User from '../User';
-import LogOut from '../../components/LogOut';
+// import Home from '../Home';
+// import PerfilPokemon from '../PerfilPokemon/PerfilPokemon'
+// import Header from '../../Components/Header/Header';
+// import Login from '../Login';
+// import Pokemon from '../Pokemons/Pokemon';
+// import NotFound from '../NotFound';
+// import Footer from '../../Components/Footer'
+// import './Main.css';
+// import User from '../User';
+// import LogOut from '../../components/LogOut';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Router>
-        <Login path="/login"/>
-        <User path="/users/:username" />
-        <LogOut path="/sair" />
-        <Pokemon path="/"/>
-        <Home path = "/home" />
-        <PerfilPokemon path = "/pokemons/:name"/>
-        <NotFound default />
-      </Router>
-      <Footer />
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <Router>
+//         <Login path="/login"/>
+//         <User path="/users/:username" />
+//         <LogOut path="/sair" />
+//         <Pokemon path="/"/>
+//         <Home path = "/home" />
+//         <PerfilPokemon path = "/pokemons/:name"/>
+//         <NotFound default />
+//       </Router>
+//       <Footer />
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
